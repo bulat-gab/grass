ab3065a9e4b0617460385591283a44316789e206
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-11-21 11:12:01 +0700

    remove garbage code

--- core/utils/logger.py
+++ core/utils/logger.py
@@ -11,11 +11,10 @@ def logging_setup():
     format_error = "<green>{time:HH:mm:ss.SS}</green> <blue>{level}</blue> | " \
                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | <level>{message}</level>"
     file_path = r"logs/"
-    # if sys.platform == "win32":
 
     logger.remove()
 
-    logger.add(file_path + f"out_{date.today().strftime('%m-%d')}.log", colorize=True,
+    logger.add(file_path + f"out_{date.today().strftime('%m-%d')}.log",
                format=format_info)
 
     logger.add(sys.stdout, colorize=True,
